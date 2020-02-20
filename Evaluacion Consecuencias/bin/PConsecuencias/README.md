7e0c82c832bb94762c5a805282e9a0753e71b1e2
nnrojasv <60190771+nnrojasv@users.noreply.github.com>
2020-02-20 10:34:11 -0300

    Add files via upload
    Ejercicio final en Paquete como corresponde

---
+++ Evaluacion Consecuencias/bin/PConsecuencias/README.md
@@ -0,0 +1,13 @@
+# ConsecuenciasE
+
+Aqui estoy subiendo los cambios del ejercicio terminado
+Tomar en cuenta que no los estoy subiendo desde eclipse
+sino que los subo de la forma que no se debe, es decir,
+directo desde mi pc al gigthub usando el upload de la 
+pagina.
+
+Para pasar el proyecto a sus eclipses, les recomiendo
+crear los nombres de las clases de forma local dentro 
+del package PConsecuencias y luego copiar el codigo de
+las clases de aqui y pegarlas en sus clases locales, 
+de esa forma me resulto a mi.
---
+++ Evaluacion Consecuencias/src/PConsecuencias/Trupallas.java
@@ -0,0 +1,59 @@
+package PConsecuencias;
+
+/**
+ * 
+ * Esta clase contiene los atributos y métodos pertenecientes a la clase Trupallas
+ * hereda la clase Carros
+ * @author: Esteban Beiza / Nadia Rojas / Pamerla Bustos.
+ * @version: 19/02/2020
+ * @see <a href="https://github.com/estebanbeiza/ConsecuenciasE.git">Versión en Github</a>
+ *
+ */
+public class Trupallas extends Carro {
+	//Atributos de la clase
+	private int nivelArmadura;
+	private String nombre;
+	
+	/**
+	 *
+	 * Constructor vacio de la clase
+	 */
+	Trupallas(){
+		super();	
+	}
+	
+	/**
+	 *
+	 * Constructor de la clase
+	 * @param nivelArmadura (valores entre 1 y 5)
+	 * @param nombre nombre de la persona que maneja
+	 */
+	Trupallas(int cantidadOcupantes, String fechaIngreso, int fila, int columna, int nivelarmadura, String nombre){
+		super(cantidadOcupantes, fechaIngreso, fila, columna);
+		this.nivelArmadura = nivelarmadura;
+		this.nombre = nombre;
+	}
+
+	public int getNivelArmadura() {
+		return nivelArmadura;
+	}
+
+	public void setNivelArmadura(int nivelArmadura) {
+		this.nivelArmadura = nivelArmadura;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Trupallas [nivelArmadura=" + nivelArmadura + ", nombre=" + nombre + "]";
+	}
+	
+
+}
